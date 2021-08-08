5eb9eaa333e99aae2bbd4e19a3e45e53a4f9c899
Mahavirkumar <42721155+Mahavirkumar@users.noreply.github.com>
2021-08-08 08:55:48 +0530

    Update MainActivity.kt

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.myapplication
-
+////testing, on commit and push code jenkins automatic build creation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
